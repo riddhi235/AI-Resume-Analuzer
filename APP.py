cde5623a84157f774e40a196be1a4fe3a7d3984b
riddhi235 <riddhi352707@gmail.com>
2026-08-16 23:27:36 +0530

    Enhance home route for resume and job description processing
    Added POST method handling to the home route for resume analysis.

--- APP.py
+++ APP.py
@@ -1,11 +1,47 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from utils.pdf_parser import extract_text_from_pdf
+from utils.skill_extractor import extract_skills
+from utils.matcher import calculate_match_score
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "AI Resume Analyzer is Working!"
+
+    if request.method == "POST":
+
+        resume = request.files["resume"]
+        job_description = request.form["job_description"]
+
+        resume_text = extract_text_from_pdf(resume)
+
+        resume_skills = extract_skills(resume_text)
+        job_skills = extract_skills(job_description)
+
+        matched_skills = [
+            skill for skill in resume_skills
+            if skill in job_skills
+        ]
+
+        missing_skills = [
+            skill for skill in job_skills
+            if skill not in resume_skills
+        ]
+
+        score = calculate_match_score(
+            resume_text,
+            job_description
+        )
+
+        return render_template(
+            "result.html",
+            score=score,
+            matched_skills=matched_skills,
+            missing_skills=missing_skills
+        )
+
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
