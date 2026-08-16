b0f3930d72529793499a4ca8f63b6610807a4a49
riddhi235 <riddhi352707@gmail.com>
2026-08-17 00:10:00 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from pdf import extract_text_from_pdf
 from skill_extractor import extract_skills
 from matches import calculate_match_score
-
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
